526460f144f9bc29c20b9a7562d34b3c7594f0b7
vrglab <vrglab1@gmail.com>
2022-10-20 15:19:51 +0200

    Objects system

---
+++ src/org/BluminEngine6/Object/BluminBehaviour.java
@@ -0,0 +1,7 @@
+package org.BluminEngine6.Object;
+
+import org.BluminEngine6.Legacy.Utils.objActionData;
+
+public abstract class BluminBehaviour extends Object{
+        protected objActionData logicsData;
+}
--- src/org/BluminEngine6/Object/Object.java
+++ src/org/BluminEngine6/Object/Object.java
@@ -1,8 +1,12 @@
 package org.BluminEngine6.Object;
 
 import org.BluminEngine6.Legacy.Utils.Logics.IObjLogic;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
-public abstract class Object extends java.lang.Object implements IObjLogic {
-    private Class<? extends Object> thisClass = getClass();
-    public String name = thisClass.getName();
+import java.io.Serializable;
+
+public abstract class Object extends java.lang.Object implements IObjLogic, Serializable {
+    private final Class<? extends Object> This = getClass();
+    public String name = This.getName();
+    public boolean Active = true;
 }
