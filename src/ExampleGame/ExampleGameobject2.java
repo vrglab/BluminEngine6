69d8fdaa8bdde163cfa07f4d6df0f781025d270f
vrglab <vrglab1@gmail.com>
2022-11-02 10:34:39 +0100

    Physics

--- src/ExampleGame/ExampleGameobject2.java
+++ src/ExampleGame/ExampleGameobject2.java
@@ -1,38 +1,25 @@
 package ExampleGame;
 
-import com.bulletphysics.linearmath.Transform;
 import org.BluminEngine6.Application;
-import org.BluminEngine6.Audio.AudioSystem;
-import org.BluminEngine6.Editor.Componants.Camera;
-import org.BluminEngine6.Editor.Rendering.ImageRenderer;
-import org.BluminEngine6.Editor.Rendering.MeshRenderer3D;
-import org.BluminEngine6.Editor.SceneManagment.SceneMannager;
 import org.BluminEngine6.Audio.AudioSource;
+import org.BluminEngine6.Editor.Rendering.MeshRenderer3D;
 import org.BluminEngine6.Legacy.Utils.Debuging.Debug;
 import org.BluminEngine6.Legacy.Utils.Input;
-import org.BluminEngine6.Legacy.Utils.Math.Vector3;
-import org.BluminEngine6.Legacy.Utils.Utils;
-import org.BluminEngine6.Models.Material;
-import org.BluminEngine6.Models.Mesh;
 import org.BluminEngine6.Models.Model;
 import org.BluminEngine6.Object.BluminBehaviour;
 import org.BluminEngine6.Physics.Collision.Collider;
-import org.BluminEngine6.Physics.Physics;
 import org.BluminEngine6.Physics.RigidBody;
 import org.BluminEngine6.Utils.ResourceBatch;
 import org.lwjgl.glfw.GLFW;
 
-import java.nio.ByteBuffer;
-import java.util.Arrays;
-
-public class ExampleGameobject extends BluminBehaviour {
+public class ExampleGameobject2 extends BluminBehaviour {
 
     Model ourm2;
     MeshRenderer3D mr;
     AudioSource as;
 
     Collider collider;
-    public RigidBody rgBody;
+    RigidBody rgBody;
 
     @Override
     public void Update() {
@@ -59,14 +46,8 @@ public class ExampleGameobject extends BluminBehaviour {
 
     @Override
     public void Awake() {
-        //Model ourm = new Model(ResourceBatch.GetMesh(0, Application.getCoreResources().getRoot().getFolder(0)), new Material());
+        transform.position.x = 10;
         ourm2 = ResourceBatch.GetModel(0, Application.getCoreResources().getRoot().getFolder(1));
-        /*try {
-            ourm.SaveToFile("Cube", Application.getTempFolder().getAbsolutePath());
-            ourm2.SaveToFile("Dragon", Application.getTempFolder().getAbsolutePath());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }*/
         mr = RegisterComponent(new MeshRenderer3D(ourm2));
 
         collider = RegisterComponent( new Collider());
--- src/ExampleGame/ExampleScene.java
+++ src/ExampleGame/ExampleScene.java
@@ -6,16 +6,22 @@ import org.BluminEngine6.Legacy.Audio.Mixer;
 import org.BluminEngine6.Editor.SceneManagment.Scene;
 import org.BluminEngine6.Legacy.Utils.Debuging.Debug;
 import org.BluminEngine6.Legacy.Utils.Input;
+import org.BluminEngine6.Physics.Physics;
 import org.lwjgl.glfw.GLFW;
 
 public class ExampleScene extends Scene {
-    ExampleGameobject ego = new ExampleGameobject();
 
-    AudioSystem ass = new AudioSystem();
+    Physics physics = new Physics();
+
+    ExampleGameobject ego = new ExampleGameobject();
+    ExampleGameobject2 egoo = new ExampleGameobject2();
+
+    AudioSystem ass;
 
     public ExampleScene() {
+        RegisterGameObject(physics);
         RegisterGameObject(ego);
-        RegisterGameObject(ass);
+        RegisterGameObject(egoo);
     }
 
     @Override
@@ -26,6 +32,15 @@ public class ExampleScene extends Scene {
         if(Input.Instance().WasPressed(GLFW.GLFW_KEY_R)) {
             ego.Disable();
         }
+
+
+        if(!ego.rgBody.getBody().checkCollideWith(egoo.rgBody.getBody())) {
+            ego.transform.position.x += 0.001f;
+        }
+
+        if(!egoo.rgBody.getBody().checkCollideWith(ego.rgBody.getBody())) {
+            egoo.transform.position.x += 0.01f;
+        }
     }
 
     @Override
@@ -35,7 +50,7 @@ public class ExampleScene extends Scene {
 
     @Override
     public void Init() {
-
+            ego.collider.Debug = true;
     }
 
     @Override
